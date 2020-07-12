db18990f43478a6119afb5bf2dc521baeb10d5cd
Sergey Grishchenko <s.i.grishchenko@gmail.com>
2020-07-13 02:01:27 +0300

    inversify modules are used for code structuring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import 'dotenv/config'
 import 'reflect-metadata'
+import {Container} from "inversify";
+import {storages, middlewares, scheduling} from "./modules";
 import {Types} from "./types";
-import {container} from './container'
 import {TelegrafBot} from "./TelegrafBot";
+
+export const container = new Container({
+    defaultScope: "Singleton",
+});
+
+container.load(
+    storages,
+    middlewares,
+    scheduling,
+);
+
+container.bind<TelegrafBot>(Types.TelegrafBot).to(TelegrafBot);
 
 container.get<TelegrafBot>(Types.TelegrafBot);
