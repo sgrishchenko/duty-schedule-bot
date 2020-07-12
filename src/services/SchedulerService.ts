59693e3c82b5dc429520e6b2b74deb624b48891f
Sergey Grishchenko <s.i.grishchenko@gmail.com>
2020-07-12 17:36:34 +0300

    fix message sending

--- src/services/SchedulerService.ts
+++ src/services/SchedulerService.ts
@@ -3,29 +3,26 @@ import {Scheduler} from "./Scheduler";
 import {bot} from "../bot";
 
 export class SchedulerService {
-   public async init() {
-       const dutySchedules = await dutyScheduleStorage.getAll();
+    public async init() {
+        const dutySchedules = await dutyScheduleStorage.getAll();
 
-       for (const entry of Object.entries(dutySchedules)) {
-           const [key, dutySchedule] = entry;
-           const chatId = Number(key);
+        for (const entry of Object.entries(dutySchedules)) {
+            const [key, dutySchedule] = entry;
+            const chatId = Number(key);
 
-           new Scheduler(chatId, dutySchedule, pointer => {
-               const {members, teamSize} = dutySchedule;
-               const team = [];
+            new Scheduler(chatId, dutySchedule, pointer => {
+                const {members, teamSize} = dutySchedule;
+                const team = [];
 
-               for (let i = 0; i < Math.min(teamSize, members.length); i++) {
-                  const memberIndex = (members.length + pointer + i) % members.length;
-                  team.push(members[memberIndex])
-               }
+                for (let i = 0; i < Math.min(teamSize, members.length); i++) {
+                    const memberIndex = (members.length + pointer + i) % members.length;
+                    team.push(members[memberIndex])
+                }
 
-               bot.context.reply(
-                   'Now on duty:' +
-                   team.join('\n')
-               )
-           })
-       }
-   }
+                bot.telegram.sendMessage(chatId, 'Now on duty:\n' + team.join('\n'))
+            })
+        }
+    }
 }
 
 export const schedulerService = new SchedulerService()
