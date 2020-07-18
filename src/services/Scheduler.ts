7b6ee662a6c312c4e02409aab92d99c09271a60a
Sergey Grishchenko <s.i.grishchenko@gmail.com>
2020-07-18 20:02:10 +0300

    fix problem with initial duty notification at the weekend

--- src/services/Scheduler.ts
+++ src/services/Scheduler.ts
@@ -1,9 +1,8 @@
-import moment from "moment";
+import moment, { now } from "moment";
 import { DutySchedule } from "../models/DutySchedule";
 import { Interval } from "../models/Interval";
 
 export class Scheduler {
-  private readonly startTimeout: NodeJS.Timeout;
   private handleTimeout?: NodeJS.Timeout;
   private pointer: number;
 
@@ -15,53 +14,54 @@ export class Scheduler {
     this.pointer = dutySchedule.pointer;
 
     const { hours, minutes } = dutySchedule.time;
+
     const now = moment().utc();
     const start = moment().utc().hours(hours).minutes(minutes);
 
-    let timeToStart = start.diff(now);
-
-    if (timeToStart < 0) {
-      start.add(1, "day");
-      timeToStart = start.diff(now);
+    if (start.isAfter(now)) {
+      start.subtract(1, "day");
     }
 
-    this.startTimeout = setTimeout(() => {
-      this.planNextHandling();
-    }, timeToStart);
+    this.planNextHandling(start.valueOf());
   }
 
-  private planNextHandling() {
+  private planNextHandling(timestamp = now()) {
     this.handleSchedule();
+
+    const now = moment();
+    const next = moment(this.getNextIntervalTime(timestamp));
+    const delayTime = next.diff(now);
 
     this.handleTimeout = setTimeout(() => {
       this.planNextHandling();
-    }, this.getIntervalMilliseconds());
+    }, delayTime);
   }
 
-  private getIntervalMilliseconds() {
+  private getNextIntervalTime(timestamp: number) {
     const { interval } = this.dutySchedule;
 
     switch (interval) {
       case Interval.Daily: {
-        return moment.duration(1, "day").asMilliseconds();
+        return moment(timestamp).add(1, "day").valueOf();
       }
       case Interval.Weekly: {
-        return moment.duration(1, "week").asMilliseconds();
+        return moment(timestamp).add(1, "week").valueOf();
       }
       case Interval.EveryWorkday: {
-        const duration = moment.duration(0);
-        const nextDay = moment().utc();
+        const date = moment(timestamp);
 
         do {
-          nextDay.add(1, "day");
-          duration.add(1, "day");
+          date.add(1, "day");
+        } while (Scheduler.isWeekend(date.valueOf()));
 
-          // while next day is Sunday of Saturday
-        } while (nextDay.isoWeekday() === 6 || nextDay.isoWeekday() === 7);
-
-        return duration.asMilliseconds();
+        return date.valueOf();
       }
     }
+  }
+
+  private static isWeekend(timestamp: number) {
+    const date = moment(timestamp).utc();
+    return date.isoWeekday() === 6 || date.isoWeekday() === 7;
   }
 
   private getMemberIndex(index: number) {
@@ -85,8 +85,6 @@ export class Scheduler {
   }
 
   public destroy() {
-    clearTimeout(this.startTimeout);
-
     if (this.handleTimeout) {
       clearInterval(this.handleTimeout);
     }
